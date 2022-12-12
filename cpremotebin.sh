28f8112f58b37d5bf060d86ff6c92a7d58843dcc
skillet <clelansc@rose-hulman.edu>
2022-12-12 06:53:15 -0500

    hw01 done

---
+++ cpremotebin.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+
+scp -r bone:/bin /home/skillet/git/ece434/BB-bin
+scp -r bone:/usr/bin /home/skillet/git/ece434/BB-bin/usr
+scp -r bone:/usr/include /home/skillet/git/ece434/BB-bin/usr
+scp -r bone:/usr/lib /home/skillet/git/ece434/BB-bin/usr
+scp -r bone:/lib /home/skillet/git/ece434/BB-bin
+
---
+++ hw01/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <ncurses.h>
+
+int main() {
+
+    initscr();
+    wclear(stdscr);
+    leaveok(stdscr, true);
+    nodelay(stdscr, false);
+    noecho();
+    keypad(stdscr, true);
+    cbreak();
+
+    int curX = 0;
+    int curY = 0;
+    int input;
+
+    printf("%i by %i\n", COLS, LINES);
+
+    while(1){
+        input = wgetch(stdscr);
+        switch(input){
+            case KEY_UP:
+                curY--;
+                break;
+            case KEY_DOWN:
+                curY++;
+                break;
+            case KEY_LEFT:
+                curX--;
+                break;
+            case KEY_RIGHT:
+                curX++;
+                break;
+            case 100:
+                clear();
+                break;
+            case 120:
+                goto exitapp;
+            default:
+                break;
+        }
+
+        curX = curX >= COLS ? COLS-1 : curX;
+        curX = curX <= 0 ? 0 : curX;
+        curY = curY >= LINES ? LINES-1 : curY;
+        curY = curY <= 0 ? 0 : curY;
+
+        wmove(stdscr, curY, curX);
+        waddstr(stdscr, "#");
+        wrefresh(stdscr);
+    }
+
+exitapp:
+    endwin();
+
+    return EXIT_SUCCESS;
+}
